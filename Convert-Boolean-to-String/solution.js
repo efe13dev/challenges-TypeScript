d3b3a49a9825612d726cb29e3acbc62ce2152249
efe <eefee1987@gmail.com>
2023-12-08 09:00:36 +0100

    add Convert a Boolean to a String

---
+++ Convert-Boolean-to-String/solution.js
@@ -0,0 +1,5 @@
+"use strict";
+const booleanToString = (b) => {
+    return b.toString();
+};
+console.log(booleanToString(true));
---
+++ Convert-Boolean-to-String/solution.ts
@@ -0,0 +1,5 @@
+const booleanToString = (b: boolean): string => {
+  return b.toString();
+};
+
+console.log(booleanToString(true));
