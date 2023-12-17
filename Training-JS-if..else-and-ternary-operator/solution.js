41d6e44772fa89e4412c2d9b3963d8c6306d4b22
efe <eefee1987@gmail.com>
2023-12-17 09:01:45 +0100

    add Training JS #7: if..else and ternary operator

---
+++ Training-JS-if..else-and-ternary-operator/solution.js
@@ -0,0 +1,11 @@
+"use strict";
+function saleHotdogs(n) {
+    if (n < 5)
+        return n * 100;
+    if (n >= 5 && n < 10)
+        return n * 95;
+    if (n >= 10)
+        return n * 90;
+    throw new Error('The method or operation is not implemented.');
+}
+console.log(saleHotdogs(9));
---
+++ Training-JS-if..else-and-ternary-operator/solution.ts
@@ -0,0 +1,8 @@
+function saleHotdogs(n: number): number {
+  if (n < 5) return n * 100;
+  if (n >= 5 && n < 10) return n * 95;
+  if (n >= 10) return n * 90;
+  throw new Error('The method or operation is not implemented.');
+}
+
+console.log(saleHotdogs(9));
