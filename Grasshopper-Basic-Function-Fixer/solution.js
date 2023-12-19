213e09b8cf126ef0f0bd7c454bf0e9f6e1bf4a73
efe <eefee1987@gmail.com>
2023-12-19 06:48:11 +0100

    add Grasshopper - Basic Function Fixer

---
+++ Grasshopper-Basic-Function-Fixer/solution.js
@@ -0,0 +1,6 @@
+"use strict";
+const addFive = (num) => {
+    let total = num + 5;
+    return total;
+};
+console.log(addFive(3));
---
+++ Grasshopper-Basic-Function-Fixer/solution.ts
@@ -0,0 +1,6 @@
+const addFive = (num: number): number => {
+  let total = num + 5;
+  return total;
+};
+
+console.log(addFive(3));
