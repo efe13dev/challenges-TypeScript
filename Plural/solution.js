965d6d19c357082409bd4cfd80a132f95863794b
efe <eefee1987@gmail.com>
2023-12-22 07:33:25 +0100

    add Plural

---
+++ Plural/solution.js
@@ -0,0 +1,5 @@
+"use strict";
+function plural(n) {
+    return n !== 1;
+}
+console.log(plural(0));
---
+++ Plural/solution.ts
@@ -0,0 +1,5 @@
+function plural(n: number): boolean {
+  return n !== 1;
+}
+
+console.log(plural(0));
