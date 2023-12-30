6d3c6fd07e6d3373e7588d4d368ad58e0b7ffb9c
efe <eefee1987@gmail.com>
2023-12-30 07:55:35 +0100

    add String repeat

---
+++ String-repeat/solution.js
@@ -0,0 +1,5 @@
+"use strict";
+function repeatStr(n, s) {
+    return s.repeat(n);
+}
+console.log(repeatStr(3, 'Hello'));
---
+++ String-repeat/solution.ts
@@ -0,0 +1,5 @@
+function repeatStr(n: number, s: string): string {
+  return s.repeat(n);
+}
+
+console.log(repeatStr(3, 'Hello'));
