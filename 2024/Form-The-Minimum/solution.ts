fb67acc4dd26f5aa5efa07ef93738104c1970fd8
efe <eefee1987@gmail.com>
2024-01-01 08:47:16 +0100

    add Form The Minimum

---
+++ 2024/Form-The-Minimum/solution.ts
@@ -0,0 +1,9 @@
+function minValue(values: number[]): number {
+  const uniqueDigits = Array.from(new Set(values)).sort((a, b) => a - b);
+
+  const result = parseInt(uniqueDigits.join(''));
+
+  return result;
+}
+
+console.log(minValue([1, 9, 3, 1, 7, 4, 6, 6, 7]));
