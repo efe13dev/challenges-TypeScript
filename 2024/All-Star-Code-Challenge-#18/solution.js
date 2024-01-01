31566bcb94c18402b56523d459c353d48f695309
efe <eefee1987@gmail.com>
2024-01-01 08:21:44 +0100

    add All Star Code Challenge #18

---
+++ 2024/All-Star-Code-Challenge-#18/solution.js
@@ -0,0 +1,11 @@
+"use strict";
+function strCount(str, letter) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === letter) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(strCount('Hello', 'l'));
