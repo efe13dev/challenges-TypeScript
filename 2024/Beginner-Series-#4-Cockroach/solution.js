8cb1e9c1049f2fffce3144d9e4155d220ee32d65
efe <eefee1987@gmail.com>
2024-01-02 17:12:07 +0100

    add Beginner Series #4 Cockroach

---
+++ 2024/Beginner-Series-#4-Cockroach/solution.js
@@ -0,0 +1,5 @@
+'use strict';
+function cockroachSpeed(s) {
+  return Math.floor(s * 27.777777777778);
+}
+console.log(cockroachSpeed(1.08));
---
+++ 2024/Beginner-Series-#4-Cockroach/solution.ts
@@ -0,0 +1,5 @@
+function cockroachSpeed(s: number): number {
+  return Math.floor(s * 27.777777777778);
+}
+
+console.log(cockroachSpeed(1.08));
