93684380ef81b84e4c20565e9d28596f61dfdc91
efe <eefee1987@gmail.com>
2024-01-02 18:06:13 +0100

    add Opposite number

---
+++ 2024/Opposite-number/solution.js
@@ -0,0 +1,5 @@
+"use strict";
+function oposite(n) {
+    return n > 0 ? -n : Math.abs(n);
+}
+console.log(oposite(-14));
---
+++ 2024/Opposite-number/solution.ts
@@ -0,0 +1,5 @@
+function oposite(n: number): number {
+  return n > 0 ? -n : Math.abs(n);
+}
+
+console.log(oposite(-14));
