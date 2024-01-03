16d30f494dde40878a13044b9a9ba855e70bffc0
efe <eefee1987@gmail.com>
2024-01-03 07:14:44 +0100

    add Flatten and sort an array

---
+++ 2024/Flatten-and-sort-an-array/solution.js
@@ -0,0 +1,7 @@
+"use strict";
+function flattenAndSort(inputArray) {
+    const flattenedArray = inputArray.flat();
+    const sortedArray = flattenedArray.sort((a, b) => a - b);
+    return sortedArray;
+}
+console.log(flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]));
