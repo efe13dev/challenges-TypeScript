072dc7988db0830e346ac7fbaa87c572163f2cb5
efe <eefee1987@gmail.com>
2024-01-23 20:32:33 +0100

    add Volume of a Cuboid

---
+++ 2024/Volume-of-a-Cuboid/solution.js
@@ -0,0 +1,5 @@
+"use strict";
+function getVolumeOfCuboid(length, width, height) {
+    return length * width * height;
+}
+console.log(getVolumeOfCuboid(52, 17, 5));
---
+++ 2024/Volume-of-a-Cuboid/solution.ts
@@ -0,0 +1,8 @@
+function getVolumeOfCuboid(
+  length: number,
+  width: number,
+  height: number
+): number {
+  return length * width * height;
+}
+console.log(getVolumeOfCuboid(52, 17, 5));
