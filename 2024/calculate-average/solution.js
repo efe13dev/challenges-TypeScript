004ca4186feac9bb2312aac70b009916500cd305
efe <eefee1987@gmail.com>
2024-06-10 17:21:54 +0200

    add calculate average

---
+++ 2024/calculate-average/solution.js
@@ -0,0 +1,11 @@
+"use strict";
+function findAverage(arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
+    else {
+        const total = arr.reduce((acc, count) => acc + count, 0);
+        return total / arr.length;
+    }
+}
+console.log(findAverage([5, 6, 8]));
