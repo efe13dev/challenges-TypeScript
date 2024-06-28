0744a0aef3648314c34d3a91a246caa587bd9bb1
efe <eefee1987@gmail.com>
2024-06-28 07:34:35 +0200

    add over the road

---
+++ 2024/over-the-road/solution.js
@@ -0,0 +1,5 @@
+"use strict";
+function overTheRoad(address, n) {
+    return 2 * n + 1 - address;
+}
+console.log(overTheRoad(2, 3));
---
+++ 2024/over-the-road/solution.ts
@@ -0,0 +1,5 @@
+function overTheRoad(address: number, n: number): number {
+  return 2 * n + 1 - address;
+}
+
+console.log(overTheRoad(2, 3));
