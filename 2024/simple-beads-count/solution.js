5f01f4869461d7efe49962d2c86c3d71bae03c31
efe <eefee1987@gmail.com>
2024-07-21 08:07:38 +0200

    add simple beads count

---
+++ 2024/simple-beads-count/solution.js
@@ -0,0 +1,2 @@
+var counterRedBeads = function (n) { return (n < 2 ? 0 : 2 * (n - 1)); };
+console.log(counterRedBeads(5));
---
+++ 2024/simple-beads-count/solution.ts
@@ -0,0 +1,3 @@
+const counterRedBeads = (n: number): number => (n < 2 ? 0 : 2 * (n - 1));
+
+console.log(counterRedBeads(5));
