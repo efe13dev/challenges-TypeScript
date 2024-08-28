b2d151ba3d06ee8ced4b4c8c348e5ed9f8515d2a
efe <eefee1987@gmail.com>
2024-08-28 07:06:26 +0200

    add compare strings

---
+++ 2024/compare-strings/solution.js
@@ -0,0 +1,23 @@
+function compare(s1, s2) {
+    function isAlphabetic(str) {
+        return /^[a-zA-Z]*$/.test(str);
+    }
+    function asciiSum(str) {
+        return str.split('').reduce(function (sum, char) { return sum + char.charCodeAt(0); }, 0);
+    }
+    s1 = s1 ? s1.toUpperCase() : '';
+    s2 = s2 ? s2.toUpperCase() : '';
+    if (!isAlphabetic(s1))
+        s1 = '';
+    if (!isAlphabetic(s2))
+        s2 = '';
+    return asciiSum(s1) === asciiSum(s2);
+}
+console.log(compare('AD', 'BC'));
+console.log(compare('AD', 'DD'));
+console.log(compare('gf', 'FG'));
+console.log(compare('zz1', ''));
+console.log(compare('ZzZz', 'ffPFF'));
+console.log(compare('kl', 'lz'));
+console.log(compare(null, ''));
+console.log(compare('', null));
---
+++ 2024/compare-strings/solution.ts
@@ -0,0 +1,26 @@
+function compare(s1: string | null, s2: string | null): boolean {
+  function isAlphabetic(str: string): boolean {
+    return /^[a-zA-Z]*$/.test(str);
+  }
+
+  function asciiSum(str: string): number {
+    return str.split('').reduce((sum, char) => sum + char.charCodeAt(0), 0);
+  }
+
+  s1 = s1 ? s1.toUpperCase() : '';
+  s2 = s2 ? s2.toUpperCase() : '';
+
+  if (!isAlphabetic(s1)) s1 = '';
+  if (!isAlphabetic(s2)) s2 = '';
+
+  return asciiSum(s1) === asciiSum(s2);
+}
+
+console.log(compare('AD', 'BC'));
+console.log(compare('AD', 'DD'));
+console.log(compare('gf', 'FG'));
+console.log(compare('zz1', ''));
+console.log(compare('ZzZz', 'ffPFF'));
+console.log(compare('kl', 'lz'));
+console.log(compare(null, ''));
+console.log(compare('', null));
