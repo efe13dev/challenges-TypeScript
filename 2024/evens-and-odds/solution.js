a0175fa2a285e5be058dd94ab21c22c79741908d
efe <eefee1987@gmail.com>
2024-09-01 19:01:55 +0200

    add evens and odds

---
+++ 2024/evens-and-odds/solution.js
@@ -0,0 +1,8 @@
+var evensAndOdds = function (n) {
+    return n % 2 === 0 ? n.toString(2) : n.toString(16);
+};
+console.log(evensAndOdds(0));
+console.log(evensAndOdds(1));
+console.log(evensAndOdds(2));
+console.log(evensAndOdds(3));
+console.log(evensAndOdds(4));
---
+++ 2024/evens-and-odds/solution.ts
@@ -0,0 +1,9 @@
+const evensAndOdds = (n: number): string => {
+  return n % 2 === 0 ? n.toString(2) : n.toString(16);
+};
+
+console.log(evensAndOdds(0));
+console.log(evensAndOdds(1));
+console.log(evensAndOdds(2));
+console.log(evensAndOdds(3));
+console.log(evensAndOdds(4));
