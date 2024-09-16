2265e00b3573b9006c9d5fd196ab508854910be4
efe <eefee1987@gmail.com>
2024-09-16 07:08:11 +0200

    add convert to binary

---
+++ 2024/convert-to-binary/solution.js
@@ -0,0 +1,7 @@
+function toBinary(n) {
+    return Number(n.toString(2));
+}
+console.log(toBinary(1));
+console.log(toBinary(2));
+console.log(toBinary(3));
+console.log(toBinary(5));
---
+++ 2024/convert-to-binary/solution.ts
@@ -0,0 +1,8 @@
+function toBinary(n: number): number {
+  return Number(n.toString(2));
+}
+
+console.log(toBinary(1));
+console.log(toBinary(2));
+console.log(toBinary(3));
+console.log(toBinary(5));
