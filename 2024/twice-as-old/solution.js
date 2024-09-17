1e7909f45f2148304b86085ff234f82132013c9a
efe <eefee1987@gmail.com>
2024-09-17 07:14:52 +0200

    add twice as old

---
+++ 2024/twice-as-old/solution.js
@@ -0,0 +1,7 @@
+function twiceAsOld(fatherAge, sonAge) {
+    return Math.abs(fatherAge - 2 * sonAge);
+}
+console.log(twiceAsOld(36, 7));
+console.log(twiceAsOld(55, 30));
+console.log(twiceAsOld(10, 10));
+console.log(twiceAsOld(42, 21));
---
+++ 2024/twice-as-old/solution.ts
@@ -0,0 +1,8 @@
+function twiceAsOld(fatherAge: number, sonAge: number): number {
+  return Math.abs(fatherAge - 2 * sonAge);
+}
+
+console.log(twiceAsOld(36, 7));
+console.log(twiceAsOld(55, 30));
+console.log(twiceAsOld(10, 10));
+console.log(twiceAsOld(42, 21));
