f1aaf98eb91ea02bcc6a595efa78cc5e451a5581
efe <eefee1987@gmail.com>
2024-09-18 07:19:17 +0200

    add compiler file to is the string to uppercase

---
+++ 2024/is-the-string-uppercase/solution.js
@@ -0,0 +1,6 @@
+var isTheStringUppercase = function (string) {
+    return string === string.toUpperCase();
+};
+console.log(isTheStringUppercase('HELLO'));
+console.log(isTheStringUppercase('Hello'));
+console.log(isTheStringUppercase('hello'));
