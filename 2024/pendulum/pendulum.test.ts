ca074e8047cda133422c6eefb48e35f0a3755716
efe <efe13dev@gmail.com>
2024-11-09 07:41:26 +0100

    fix test

--- 2024/pendulum/pendulum.test.ts
+++ 2024/pendulum/pendulum.test.ts
@@ -2,6 +2,9 @@ import { describe, it, expect } from 'bun:test';
 import { pendulum } from './pendulum';
 
 describe('pendulum', () => {
+  it('should be return an array', () => {
+    expect(pendulum([5, 4, 6, 7, 8])).toBeInstanceOf(Array);
+  });
   it('should return the correct pendulum arrangement', () => {
     expect(pendulum([5, 4, 6, 7, 8])).toEqual([8, 6, 4, 5, 7]);
   });
